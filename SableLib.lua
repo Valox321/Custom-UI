45411c79c1ba7e965e9992207eb1f5a73f2327f8
Valox321 <Valox321@users.noreply.github.com>
2026-09-25 15:35:12 +0200

    sable: version 1.6-tabfix

--- SableLib.lua
+++ SableLib.lua
@@ -7,7 +7,7 @@
 --// Page:AddToggle({ Name = "...", Description = "...", Default = false, Callback = function(v) end })
 
 local SableLib = {}
-SableLib.Version = "1.5-bild-look"
+SableLib.Version = "1.6-tabfix"
 
 local Players = game:GetService("Players")
 local TweenService = game:GetService("TweenService")
